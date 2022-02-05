34c35a04f36b0263eb3f3c920db8db69d5d5fc2d
Letisia-Ornelle <57174957+Letisia-Ornelle@users.noreply.github.com>
2022-02-05 12:21:08 +0100

    Aggiunta di metodi get e set relativi al nome della Label

--- Home2/src/main/java/home/home2/PendentScreen.java
+++ Home2/src/main/java/home/home2/PendentScreen.java
@@ -2,6 +2,7 @@ package home.home2;
 
 public class PendentScreen {
     private String screen;
+    private String label;
 
     public PendentScreen() {
         screen = "";
@@ -18,14 +19,25 @@ public class PendentScreen {
     }
 
     public Boolean isNull() {
-        if (screen.equals("")) {
+        if(screen.equals("")) {
             return true;
         }
         return false;
     }
 
+    /*Bisogna definire set e get dell'immagine per la schermata result*/
+
     public void clear() {
         screen = "";
+    }
+
+
+    public String getLabel(){
+        return label;
+    }
+
+    public void setLabel(String label){
+        this.label = label;
     }
 
 }
